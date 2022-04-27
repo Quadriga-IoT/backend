7429fc170444fa12c17f35b3cd296cb19fdd2238
LAPTOP-VC20NCRF\Zeynep <zeynepipekk1@outlook.com>
2022-04-27 22:20:47 +0300

    MQTT controller oluşturuldu ama kodda hata var. Eklenmesi gerekenler var  (HATALI).

---
+++ controllers/mqttController.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt');
+const mongoose= require('mongoose');
+
+const host = 'localhost'
+const port = '80'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+const topic = '/tractor/mqtt'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+  client.publish(topic, 'tractor deneme', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
+
+module.exports = client.on;
